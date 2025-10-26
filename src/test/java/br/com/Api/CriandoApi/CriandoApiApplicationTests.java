8f2ef054af28c75e79d6c7e9384ec8a3d4ed22b3
zxDuarte1 <165347640+zxDuarte1@users.noreply.github.com>
2025-10-26 15:20:57 -0300

    Add files via upload

---
+++ src/test/java/br/com/Api/CriandoApi/CriandoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.Api.CriandoApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CriandoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
